e9408833480cbe9a39daee21c222967da2fabb89
MDAddario <michael.lindner.daddario@gmail.com>
2020-04-25 18:53:45 -0400

    Fixes issues with data typing

--- C_data_structures/array_list.c
+++ C_data_structures/array_list.c
@@ -2,38 +2,38 @@
 #include "array_list.h"
 
 // Constructor
-AL* new_AL(int capacity) {
-	
+AL* new_AL(ATYPE capacity) {
+
 	// Allocate the memory for an array list
 	AL* list = (AL*)malloc(sizeof(AL));
-	
+
 	// Format the capacity
 	if (capacity < 1)
 		list->capacity = 10;
 	else
 		list->capacity = capacity;
-	
+
 	// Create the hidden array
 	list->array = (DTYPE*)malloc(list->capacity * sizeof(DTYPE));
-	
+
 	// Start with zero elements
 	list->size = 0;
 	return list;
 }
 
 // Add element to the array list at given position
-void AL_add_at(AL* list, DTYPE value, int index) {
-	
+void AL_add_at(AL* list, DTYPE value, ATYPE index) {
+
 	// Expand array if needed
 	if (list->size == list->capacity) {
 		list->capacity *= 2;
 		list->array = (DTYPE*)realloc(list->array, list->capacity * sizeof(DTYPE));
 	}
-	
+
 	// Displace all elements that are after the index
-	for (int j = list->size - 1; j >= index; j--)
+	for (ATYPE j = list->size - 1; j >= index; j--)
 		list->array[j + 1] = list->array[j];
-	
+
 	// Add the value
 	list->array[index] = value;
 	list->size++;
@@ -42,7 +42,7 @@ void AL_add_at(AL* list, DTYPE value, int index) {
 
 // Add element to the start of the array list
 void AL_add_start(AL* list, DTYPE value) {
-	
+
 	// Add to position zero
 	AL_add_at(list, value, 0);
 	return;
@@ -50,86 +50,85 @@ void AL_add_start(AL* list, DTYPE value) {
 
 // Add element to the end of the array list
 void AL_add_end(AL* list, DTYPE value) {
-	
-	// Add to position zero
+
+	// Add to last position
 	AL_add_at(list, value, list->size);
 	return;
 }
 
 // Clear the list
 void AL_clear(AL* list) {
-	
+
 	// Set size to zero
 	list->size = 0;
 	return;
 }
 
 // Check if the list contains a value
-int AL_contains(AL* list, DTYPE value) {
-	
+BOOL AL_contains(AL* list, DTYPE value) {
+
 	// Find index value
-	int index = AL_index_of(list, value);
-	
+	ATYPE index = AL_index_of(list, value);
+
 	// Value not in list
 	if (index == -1)
-		return 0
-	
+		return 0;
+
 	// Value in list
 	return 1;
 }
-}
 
 // Return an element from the list
-DTYPE AL_get(AL* list, int index) {
-	
+DTYPE AL_get(AL* list, ATYPE index) {
+
 	// Retrieve element from array
 	return list->array[index];
 }
 
 // Return the index corresponding to a value
-int AL_index_of(AL* list, DTYPE value) {
-	
+ATYPE AL_index_of(AL* list, DTYPE value) {
+
 	// Scan the array
-	for (int j = 0; j < list->size; j++)
+	for (ATYPE j = 0; j < list->size; j++)
 		if (list->array[j] == value)
 			return j;
 	return -1;
 }
 
 // Determine if array is empty
-int AL_is_empty(AL* list) {
+BOOL AL_is_empty(AL* list) {
 
-	return !array->size;
+	return !list->size;
 }
 
 // Remove element at given index
-void AL_remove_at(AL* list, int index) {
-	
+void AL_remove_at(AL* list, ATYPE index) {
+
 	// Shift all the elements back
-	for (int j = index; j < list->size; j++)
+	for (ATYPE j = index; j < list->size; j++)
 		list->array[j] = list->array[j + 1];
-	
+
 	// Reduce the size
 	list->size--;
 	return;
 }
 
 // Remove a given value from list
-int AL_remove_value(AL* list, DTYPE value) {
-	
+BOOL AL_remove_value(AL* list, DTYPE value) {
+
 	// Find index value
-	int index = AL_index_of(list, value);
-	
+	ATYPE index = AL_index_of(list, value);
+
 	// Value not in list
 	if (index == -1)
-		return 0
-	
+		return 0;
+
 	// Value in list
-	AL_remove_at(list, j);
+	AL_remove_at(list, index);
 	return 1;
 }
 
 // Determine the size of the list
-int AL_size(AL* list) {
+ATYPE AL_size(AL* list) {
 	return list->size;
 }
--- C_data_structures/array_list.h
+++ C_data_structures/array_list.h
@@ -1,5 +1,7 @@
-#define BOOL int
-#define DTYPE int
+#define BOOL char   // Treat booleans
+#define DTYPE long  // Data type of objects to store
+#define ATYPE short // Data type for the size of the underlying array
+
 /*
  *   NOTE THAT THE DTYPE MUST BE COMPARABLE VIA == OPERATION
  */
@@ -7,21 +9,21 @@
 // Struct for the array list
 typedef struct struct_AL {
 
-	DTYPE* array;		// Underlying array
-	int    size;		// Number of elements in the array
-	int    capacity;	// Total capacity of the array
-	
+	DTYPE* array;       // Underlying array
+	ATYPE  size;        // Number of elements in the array
+	ATYPE  capacity;    // Total capacity of the array
+
 } AL;
 
-AL*   new_AL(                                 int capacity);
-void  AL_add_at(       AL* list, DTYPE value, int index);
+AL*   new_AL(                                 ATYPE capacity);
+void  AL_add_at(       AL* list, DTYPE value, ATYPE index);
 void  AL_add_start(    AL* list, DTYPE value);
 void  AL_add_end(      AL* list, DTYPE value);
 void  AL_clear(        AL* list);
 BOOL  AL_contains(     AL* list, DTYPE value);
-DTYPE AL_get(          AL* list,              int index);
-int   AL_index_of(     AL* list, DTYPE value);
+DTYPE AL_get(          AL* list,              ATYPE index);
+ATYPE AL_index_of(     AL* list, DTYPE value);
 BOOL  AL_is_empty(     AL* list);
-void  AL_remove_at(    AL* list,              int index);
+void  AL_remove_at(    AL* list,              ATYPE index);
 BOOL  AL_remove_value( AL* list, DTYPE value);
-int   AL_size(         AL* list);
+ATYPE AL_size(         AL* list);
