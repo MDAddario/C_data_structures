a6543046fc38c24cdf73ac87937f658f2bc6150c
MDAddario <michael.lindner.daddario@gmail.com>
2020-04-25 19:32:02 -0400

    Adds more unit tests

--- C_data_structures/tests.c
+++ C_data_structures/tests.c
@@ -31,6 +31,8 @@ void assert_BOOL(BOOL a, BOOL b, char* title) {
 
 int main() {
 
+/*****************************************************************/
+
 	// Conduct array list unit tests
 	printf("===== CONDUCTING ARRAY LIST UNIT TESTS =====\n");
 
@@ -47,6 +49,50 @@ int main() {
 
 	for (ATYPE j = 0; j < capacity; j++)
 		list->array[j] = 0;     // Ensure no segfaults
+
+	// free_AL()
+	AL* list_2 = new_AL(capacity);
+	free_AL(list_2);
+
+	// AL_add_start()
+	for (ATYPE size = 1; size <= capacity; size++) {
+
+		DTYPE value = (DTYPE)size + 100;
+		AL_add_start(list, value);
+		assert_ATYPE(list->size, size, "AL_add_start()");
+		assert_ATYPE(list->capacity, capacity, "AL_add_start()");
+	}
+
+	// AL_get()
+	for (ATYPE index = 0; index < capacity; index++) {
+
+		DTYPE value = AL_get(list, index);
+		assert_DTYPE(value, 100 + capacity - index, "AL_get()");
+		assert_ATYPE(list->size, capacity, "AL_get()");
+		assert_ATYPE(list->capacity, capacity, "AL_get()");
+	}
+
+	// AL_add_end()
+	for (ATYPE size = 1; size <= capacity; size++) {
+
+		DTYPE value = (DTYPE)size + 200;
+		AL_add_end(list, value);
+		assert_ATYPE(list->size, size + capacity, "AL_end()");
+		assert_ATYPE(list->capacity, 2 * capacity, "AL_end()");
+	}
+
+	// AL_get()
+	for (ATYPE index = 0; index < capacity; index++) {
+
+		DTYPE value = AL_get(list, index + capacity);
+		assert_DTYPE(value, 200 + index + 1, "AL_get()");
+		assert_ATYPE(list->size, 2 * capacity, "AL_get()");
+		assert_ATYPE(list->capacity, 2 * capacity, "AL_get()");
+	}
+
+
+
+/*****************************************************************/
 
 	// All tests pass
 	printf("===== ALL TESTS HAVE PASSED! =====\n");
