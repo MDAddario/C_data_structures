6ebefcbd140676a1b20d987e6a220d76f5afe19d
MDAddario <michael.lindner.daddario@gmail.com>
2020-05-02 01:25:29 -0400

    Creates wrapper classes for int, double, string

--- C_data_structures/define.c
+++ C_data_structures/define.c
@@ -2,22 +2,90 @@
 #include <string.h>
 #include "define.h"
 
-BOOL short_equals(const void * a, const void * b) {
-	return (*(short*)a == *(short*)b);
+// Constructors
+Integer* new_integer(int value) {
+
+	// Allocate memory
+	Integer* wrapper = (Integer*)malloc(sizeof(Integer));
+
+	// Init field
+	wrapper->value = value;
+
+	return wrapper;
 }
 
-BOOL int_equals(const void * a, const void * b) {
-	return (*(int*)a == *(int*)b);
+Double* new_double(double value) {
+
+	// Allocate memory
+	Double* wrapper = (Double*)malloc(sizeof(Double));
+
+	// Init field
+	wrapper->value = value;
+
+	return wrapper;
 }
 
-BOOL long_equals(const void * a, const void * b) {
-	return (*(long*)a == *(long*)b);
+String* new_string(char* value) {
+
+	// Allocate memory
+	String* wrapper = (String*)malloc(sizeof(String));
+
+	// Copy the field value
+	long size = strlen(value) + 1l; // Include the null character ofc
+	wrapper->value = (char*)malloc(size * sizeof(char));
+	strcpy(wrapper->value, value);
+
+	return wrapper;
+}
+
+// Comparison methods
+BOOL integer_equals(const void * a, const void * b) {
+
+	// Cast into proper type
+	Integer* one = (Integer*)a;
+	Integer* two = (Integer*)b;
+
+	// Compare
+	return (one->value == two->value);
 }
 
 BOOL double_equals(const void * a, const void * b) {
-	return (fabs(*(double*)a - *(double*)b) < EPSILON);
+
+	// Cast into proper type
+	Double* one = (Double*)a;
+	Double* two = (Double*)b;
+
+	// Compare
+	return (fabs(one->value - two->value) < EPSILON);
 }
 
 BOOL string_equals(const void * a, const void * b) {
-	return !strcmp((char*)a, (char*)b);
+
+	// Cast into proper type
+	String* one = (String*)a;
+	String* two = (String*)b;
+
+	// Compare
+	return !strcmp(one->value, two->value);
 }
+
+// Recyclers
+void free_integer(Integer* wrapper) {
+
+	free(wrapper);
+	return;
+}
+
+void free_double(Double* wrapper) {
+
+	free(wrapper);
+	return;
+}
+
+void free_string(String* wrapper) {
+
+	free(wrapper->value);
+	free(wrapper);
+	return;
+}
+
--- C_data_structures/define.h
+++ C_data_structures/define.h
@@ -4,8 +4,36 @@
 #define INDEX_NOT_FOUND -1
 #define EPSILON         0.000001
 
-BOOL      short_equals (const void * a, const void * b);
-BOOL      int_equals   (const void * a, const void * b);
-BOOL      long_equals  (const void * a, const void * b);
-BOOL      double_equals(const void * a, const void * b);
-BOOL      string_equals(const void * a, const void * b);
+// Wrapper classes
+typedef struct struct_integer {
+
+	int entry;
+
+} Integer;
+
+typedef struct struct_double {
+
+	double entry;
+
+} Double;
+
+typedef struct struct_string {
+
+	char* entry;
+
+} String;
+
+// Constructors
+Integer*  new_integer(int    value);
+Double*   new_double (double value);
+String*   new_string (char*  value);
+
+// Comparison methods
+BOOL  integer_equals(const void * a, const void * b);
+BOOL  double_equals (const void * a, const void * b);
+BOOL  string_equals (const void * a, const void * b);
+
+// Recyclers
+void free_integer(Integer* wrapper);
+void free_double (Double*  wrapper);
+void free_string (String*  wrapper);
